2d83b9b33e0986aa887e2de4e9e02522f5a78368
NoppachaiPokma <89395590+NoppachaiPokma@users.noreply.github.com>
2021-09-21 20:15:32 +0700

    Delete script2.js

--- script2.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector('div').style.animation= "move 5s forwards";
-    document.querySelector('.back').onclick= function(){
-        document.querySelector('body').style.animation= "movee 250ms forwards";
-      }
-})
